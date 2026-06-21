feca26610093bea1303abd54bd0fa038f912e39f
firehawk039 <firehawk039@gmail.com>
2026-06-22 04:08:46 +0530

    updated

--- Index.js
+++ Index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express(); 
 const PORT = 8001
+const {connectToDB} = require("./connect");
+
+const urlRoute  = require("./routes/url");
+
+
+connectToDB("mongodb://127.0.0.1:27017/short-url")
+.then(() => console.log("Connected to DB"))
+
+app.use(express.json());
+app.use('/url', urlRoute);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
