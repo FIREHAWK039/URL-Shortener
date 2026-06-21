02a8722f67fb5d4ac2e090e593f814adc2ce409b
firehawk039 <firehawk039@gmail.com>
2026-06-22 05:22:44 +0530

    working

--- Index.js
+++ Index.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 const PORT = 8001
-const {connectToDB} = require("./connect");
+const { connectToDB } = require("./connect");
 
-const urlRoute  = require("./routes/url");
-
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 connectToDB("mongodb://127.0.0.1:27017/short-url")
-.then(() => console.log("Connected to DB"))
+    .then(() => console.log("Connected to DB"))
 
 app.use(express.json());
 app.use('/url', urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const Entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: { timestamp: Date.now() }
+        },
+    }
+    )
+    res.redirect(Entry.redirectUrl)
+})
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const {handleGenerateNewShortURL} = require("../controllers/url");
-
+const {handleGetAnalytics} = require("../controllers/url");
 
 router.post("/", handleGenerateNewShortURL);
-
+router.get('/analytics/:shortId', handleGetAnalytics);
 module.exports = router;
