e4f72f0ee8ecacb04d0c8bafa2d9ad7ecca99f93
firehawk039 <firehawk039@gmail.com>
2026-06-24 03:40:44 +0530

    error still

--- Index.js
+++ Index.js
@@ -6,21 +6,29 @@ const path = require("path");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 
+const staticRouter = require("./routes/staticRouter");
+
 connectToDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log("Connected to DB"))
 
 app.set('view engine', 'ejs');
-app.set('views', path.resolve("./views"))
+app.set('views', path.resolve("./views",))
 
 
 app.get('/test', async(req, res) => {
-    const allUrls = await URL.find({});
+    const allURL = await URL.find({});
     return res.render("home")
 
 })
 
 app.use(express.json());
+app.use(express.urlencoded({ extended:  false }));
+
+
 
 app.use('/url', urlRoute);
+
+
+app.use("/", staticRouter);
 
 app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
