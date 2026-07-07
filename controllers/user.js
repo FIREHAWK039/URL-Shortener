d40cf00cc7fba172492932997cd6986b608cd24c
firehawk039 <firehawk039@gmail.com>
2026-07-07 20:57:39 +0530

    www

--- controllers/user.js
+++ controllers/user.js
@@ -26,8 +26,9 @@ async function handleUserLogin(req, res) {
 
     
         const token = setUser(user)
-        res.cookie("uid", token)
-        return res.redirect("/")
+        res.cookie("uid", token, {
+domain: "www.google.com"
+        })
 
     return res.redirect("/")
 }
